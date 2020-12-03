d5979372e55a47b89706b86d9a8ba00e72eaf96d
shubham9146 <75287930+shubham9146@users.noreply.github.com>
2020-12-03 17:40:11 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaPrograms {
+
+   
+    public static void main(String[] args) {
+        Scanner ob = new Scanner (System.in);
+        String name;
+        System.out.println("Enter your name :");
+        name = ob.nextLine();
+        System.out.println("Hello " +name);
+       
+    }
+    
+}
