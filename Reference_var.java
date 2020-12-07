93b214af0a2e125a821d98e4397b31b095e2bc98
Shubham Kapse <75287930+shubham9146@users.noreply.github.com>
2020-12-07 08:45:30 +0530

    Create Reference_var.java
    In this type of variable, when we assign one variable to another, then reference is copied, Both variable points to same memory block.

---
+++ Reference_var.java
@@ -0,0 +1,29 @@
+package javaprograms;
+
+class Test {
+
+    int i, j;
+
+    void setij(int x, int y) {
+        i = x;
+        j = y;
+    }
+
+    void add() {
+        System.out.println(i + j);
+    }
+}
+
+public class Reference {
+
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.setij(10, 20);
+        Test t2;
+        t2 = t1;
+        t2.setij(30, 40);
+        t1.add();
+        t2.add();
+    }
+
+}
