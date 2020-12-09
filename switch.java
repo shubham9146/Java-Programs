0f89b83d56d00c862bf92bfcae0d09a9df0cb8cb
Shubham Kapse <75287930+shubham9146@users.noreply.github.com>
2020-12-09 08:13:35 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,21 @@
+public class Switch {
+
+    public static void main(String[] args) {
+        int i = 2;
+        switch (i) {
+            case 1:
+                System.out.println("Case 1");
+                break;
+            case 2:
+                System.out.println("Case 2");
+                break;
+            case 3:
+                System.out.println("Case 3");
+                break;
+            default:
+                System.out.println("DEFAULT");
+                break;
+        }
+    }
+
+}
